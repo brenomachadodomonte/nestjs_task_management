677d4d0e53112a01ac44025071e6ac94818fcf28
brenomachado <breno.monte@triadsystems.com.br>
2021-10-17 11:24:59 -0300

    Adicionando validações

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,7 +1,6 @@
 import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { Task, TaskStatus } from './task.model';
-import { title } from 'process';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
 
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Task, TaskStatus } from './task.model';
 import { v4 as uuid } from 'uuid';
 import { CreateTaskDto } from './dto/create-task.dto';
@@ -29,7 +29,12 @@ export class TasksService {
     }
 
     getTaskById(id: string): Task {
-        return this.tasks.find((t)=> t.id === id);
+        const task = this.tasks.find((t)=> t.id === id);
+        if(!task){
+            throw new NotFoundException(`Task with id ${id} not found!`);
+        }
+
+        return task;
     }
 
     createTask(createTaskDto: CreateTaskDto): Task {
@@ -47,7 +52,7 @@ export class TasksService {
     }
 
     deleteTask(id: string): Task {
-        const task = this.tasks.find((t)=> t.id === id);
+        const task = this.getTaskById(id);
         this.tasks = this.tasks.filter((t) => t.id !== id);
         return task;
     }
