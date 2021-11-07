1f0ab1a2b0f1b0d4dc0ffc84f275c109a2ae68d0
brenomachado <breno.monte@triadsystems.com.br>
2021-11-07 17:19:51 -0300

    Creating serch method

--- src/tasks/task.repository.ts
+++ src/tasks/task.repository.ts
@@ -16,7 +16,19 @@ export class TasksRepository extends Repository<Task> {
     }
 
     async getTasks(filterDto: GetTasksFilterDto): Promise<Task[]> {
+        const { status, search } = filterDto;
         const query = this.createQueryBuilder('task');
+
+        if(status) {
+            query.andWhere('task.status = :status', { status })
+        }
+
+        if(search) {
+            query.andWhere(
+                'LOWER(task.title) LIKE LOWER(:search) OR LOWER(task.description) LIKE LOWER(:search)', 
+                { search: `%${search}%` }
+            );
+        }
 
         const tasks = await query.getMany();
 
