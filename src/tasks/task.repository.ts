cf22409cc9ad97e66c74bf6170b768a3e90a6999
brenomachadodomonte <brenomachadodomonte@gmail.com>
2021-11-28 18:01:33 -0300

    Changing create Task

--- src/tasks/task.repository.ts
+++ src/tasks/task.repository.ts
@@ -1,3 +1,4 @@
+import { User } from "src/auth/user.entity";
 import { EntityRepository, Repository } from "typeorm";
 import { CreateTaskDto } from "./dto/create-task.dto";
 import { GetTasksFilterDto } from "./dto/get-tasks-filter.dto";
@@ -7,9 +8,9 @@ import { Task } from "./task.entity";
 @EntityRepository(Task)
 export class TasksRepository extends Repository<Task> {
     
-    async createTask(createTaskDto: CreateTaskDto): Promise<Task> {
+    async createTask(createTaskDto: CreateTaskDto, user: User): Promise<Task> {
         const { title, description } = createTaskDto;
-        const task = this.create({ title, description, status: TaskStatus.OPEN})
+        const task = this.create({ title, description, user, status: TaskStatus.OPEN })
         
         await this.save(task);
         return task;
